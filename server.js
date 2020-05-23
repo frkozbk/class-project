e056f29891aa9d90debf0362cf0ea8f94dc50535
faruk.ozbek <faruk.ozbek@n11.com>
2020-05-23 23:14:42 +0300

    added heroku script

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const post = require('./routes/api/post');
 const comment = require('./routes/api/comment');
 const Classroom = require('./models/Classroom');
 const Message = require('./models/Message');
-
+const path = require('path');
 const app = express();
 console.clear();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -62,6 +62,13 @@ io.on('connection', (socket) => {
 });
 
 server.listen(5001, () => `io connected`);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
